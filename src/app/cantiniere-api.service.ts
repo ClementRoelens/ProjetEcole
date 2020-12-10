124b3740b1a2069e659a30a73479ee5e64fc7237
Umberto <umberto@wegravit.com>
2020-12-10 17:20:17 +0100

    finitions

--- src/app/cantiniere-api.service.ts
+++ src/app/cantiniere-api.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const BASE_URL = "http://localhost:8080/lunchtime/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class CantiniereAPIService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  forgotPassword(email: string){
+    return this.http.post(BASE_URL + "forgotpassword?email=" + email, null)
+  }
+
 }

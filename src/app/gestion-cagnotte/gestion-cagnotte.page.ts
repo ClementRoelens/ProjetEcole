3f7d95b6dd0e4a080de3a1af8c2fa1f0a33aa8a3
JsPlayTv <schottey.julien@gmail.com>
2020-12-11 16:49:26 +0100

    Navigation Détail User

--- src/app/gestion-cagnotte/gestion-cagnotte.page.ts
+++ src/app/gestion-cagnotte/gestion-cagnotte.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { userInfo } from 'os';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-gestion-cagnotte',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionCagnottePage implements OnInit {
 
-  constructor() { }
+  unNomUser : string = "";
+  unPrenomUser : string = "";
+  unEmailUser : string = "";
+  unTelUser : string = "";
+  uneCagnotteUser : string ="";
+
+
+  ListUsers:User[] = [
+    { id:0, name:"Jean", firstname:"Transcène", password:"pwd", email:"Jean.Transcène@gmail.com", phone: "0650231845", wallet: 45},
+    { id:0, name:"Sarah", firstname:"Croche", password:"pwd", email:"Sarah.Croche@gmail.com", phone: "0650231845", wallet: 2},
+    { id:0, name:"Alex", firstname:"Tèrieure", password:"pwd", email:"Alex.Tèrieure@gmail.com", phone: "0650231845", wallet: 780}
+  ]
+
+
+
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    let UserId:string = this.route.snapshot.paramMap.get("id");
+    this.unNomUser=this.ListUsers[UserId].name;
+    this.unPrenomUser=this.ListUsers[UserId].firstname;
+    this.unEmailUser=this.ListUsers[UserId].email;
+    this.unTelUser=this.ListUsers[UserId].phone;
+    this.uneCagnotteUser=this.ListUsers[UserId].wallet;
+    
   }
 
 }
--- src/app/gestion-user/gestion-user.page.ts
+++ src/app/gestion-user/gestion-user.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GestionCagnottePage } from '../gestion-cagnotte/gestion-cagnotte.page';
 import { Router, RouterLink } from '@angular/router';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-gestion-user',
@@ -11,11 +12,15 @@ export class GestionUSerPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  id: number;
 
-  LanceCagnotte(){
-    
+  ListUsers:User[] = [
+    { id:0, name:"Jean", firstname:"Transcène", password:"pwd", email:"Jean.Transcène@gmail.com", phone: "0650231845", wallet: 45},
+    { id:0, name:"Sarah", firstname:"Croche", password:"pwd", email:"Sarah.Croche@gmail.com", phone: "0650231845", wallet: 2},
+    { id:0, name:"Alex", firstname:"Tèrieure", password:"pwd", email:"Alex.Tèrieure@gmail.com", phone: "0650231845", wallet: 780}
+  ]
+
+  ngOnInit() {
   }
 
 }

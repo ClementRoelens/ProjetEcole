31c59a8b0c62a06c35933c13b2a4b31fc0908330
ClementRoelens <clement.roelens@hotmail.com>
2020-12-17 07:22:47 +0100

    Presque fini

--- src/app/inscription/inscription.module.ts
+++ src/app/inscription/inscription.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,8 @@ import { HeaderComponent } from '../header/header.component';
   imports: [
     CommonModule,
     FormsModule,
+    FormsModule,
+     ReactiveFormsModule,
     IonicModule,
     InscriptionPageRoutingModule
   ],
--- src/app/inscription/inscription.page.ts
+++ src/app/inscription/inscription.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 import { CantiniereAPIService } from '../cantiniere-api.service';
 import { User } from '../model/user';
+import { EmailUtils } from '../utils/EmailUtils';
+import { ToastUtils } from '../utils/ToastUtils';
 
 @Component({
   selector: 'app-inscription',
@@ -8,20 +12,66 @@ import { User } from '../model/user';
   styleUrls: ['./inscription.page.scss'],
 })
 export class InscriptionPage implements OnInit {
-  user:User = {
-    wallet:0,
-    email:"exemple@exemple.com",
-    password:"test",
-    sex:2
+  user: User = {
+    name: undefined,
+    firstname: undefined,
+    email: undefined,
+    password: undefined,
+    wallet: 0,
+    sex: undefined
   };
+  userForm: FormGroup;
+  isValid: boolean = false;
 
-  constructor(private service:CantiniereAPIService) { }
+  constructor(private formBuilder: FormBuilder, private service: CantiniereAPIService, private toastController: ToastController) { }
 
   ngOnInit() {
+
+    this.userForm = this.formBuilder.group({
+      name: "",
+      firstname: "",
+      sex: "",
+      email: "",
+      password: "",
+      passwordCheck: ""
+    });
+
+    this.userForm.valueChanges.subscribe(values => {
+      let value = this.userForm.value;
+      let tempValid: boolean = true;
+      for (let property in value) {
+        if (!value[property]) {
+          tempValid = false;
+          break;
+        }
+      }
+      this.isValid = tempValid;
+    });
+
+
   }
 
-  register(){
-    
+  register() {
+    let value = this.userForm.value;
+    if (this.userForm.value.password === this.userForm.value.passwordCheck) {
+      for (let property in this.userForm.value){
+        if (property != "passwordCheck"){
+          this.user[property] = this.userForm.value[property];
+        }
+      }
+      this.service.register(this.user).subscribe((res:any) => {
+        console.log(res.status);
+      });
+      // if (EmailUtils.verifyEmail(value.email)) {
+       
+      // }
+      // else {
+      //   ToastUtils.presentToast("Veuillez saisir un email valide", "danger", this.toastController);
+      // }
+    }
+    else {
+      ToastUtils.presentToast("Les deux mots de passe ne sont pas identiques", "danger", this.toastController);
+    }
   }
 
 }
